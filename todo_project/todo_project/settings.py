5a336ee42a57438c771cc386a99ab07b75618172
Tech-Wolf07 <yammu2005@gmail.com>
2025-08-24 18:13:05 +0530

    Production configurations set

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yq5#1=-txc(y)x*rna^fk*#ws)v-j*n95)x6b&_uskxo)rw*u*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['YameenShaikh.pythonanywhere.com']
 
 
 # Application definition
