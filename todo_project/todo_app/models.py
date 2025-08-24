097b8f7f431414f767e155c94c6fd8c50e784f1d
Tech-Wolf07 <yammu2005@gmail.com>
2025-08-24 23:19:49 +0530

    Added username

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,10 +1,49 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
+class UserManager(BaseUserManager):
+    """Define a model manager for User model."""
+
+    def _create_user(self, username, email, password=None, **extra_fields):
+        """Create and save a User with the given username, email and password."""
+        if not username:
+            raise ValueError('The given username must be set')
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        """Create and save a SuperUser with the given username, email and password."""
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(username, email, password, **extra_fields)
+
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+    email = models.EmailField(('email address'), unique=True)
+
+    # Make sure USERNAME_FIELD is set to 'username'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.username
 
 class Category(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
