92b98d4359292ad240b1f530a6bf873c6f2cf223
Yameen Shaikh <yameen.naushad@outlook.com>
2025-08-24 23:02:34 +0530

    Update settings.py

--- todo_project/todo_project/settings.py
+++ todo_project/todo_project/settings.py
@@ -120,8 +120,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
+STATIC_ROOT = '/home/YameenShaikh/TodoWithAnalytics/static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
